6abb13c727ce36ca875bef1dfc87ecde631f1e71
ElliotKoch <kochelliotpro@gmail.com>
2025-05-23 17:41:30 +0200

    add comments

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import tkinter as tk
-from KnightGame import KnightGame
-
-
+from KnightGame import KnightGame  # Import the KnightGame class from the game 
 
 if __name__ == "__main__":
+    # Create the main application window
     root = tk.Tk()
+    
+    # Instantiate the KnightGame class, passing the main window as master
     menu = KnightGame(root)
+    
+    # Start the Tkinter event loop to run the application
     root.mainloop()
